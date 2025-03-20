ac35a79eec08e30b14094895ba187475ac87bf4e
Amman Soomro <63865428+ammansoomro@users.noreply.github.com>
2025-03-20 08:04:47 +0500

    Server: DB | Updated DB Connection Function

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,10 +3,7 @@ require('dotenv').config();
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('✅ Connected to MongoDB!');
     } catch (error) {
         console.error('❌ MongoDB Connection Error:', error);
