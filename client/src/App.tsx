45f29647b199619650468d184f2b3b077c403fa1
Amman Soomro <63865428+ammansoomro@users.noreply.github.com>
2025-03-21 08:16:21 +0500

    Client: Routes | Make Router Session Based

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,26 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContext/AuthContext";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 
 export default function App() {
-  const isLoggedIn = false;
+  const { isAuthenticated } = useAuth();
 
   return (
     <Routes>
-      <Route path="/" element={isLoggedIn ? <></> : <Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route
+        path="/"
+        element={isAuthenticated ? <h1>Welcome Home</h1> : <Navigate to="/login" />}
+      />
+      
+      <Route
+        path="/login"
+        element={isAuthenticated ? <Navigate to="/" /> : <Login />}
+      />
+      <Route
+        path="/signup"
+        element={isAuthenticated ? <Navigate to="/" /> : <Signup />}
+      />
     </Routes>
   );
 }
