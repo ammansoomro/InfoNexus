2244526f2c10a0e7b7418f1c6ad3848a5ca92239
muhammad-amman <63865428+ammansoomro@users.noreply.github.com>
2025-03-22 16:35:03 +0500

    Client: Routes | Updated Routes Structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,8 @@
-import { useAuth } from "./context/AuthContext/AuthContext";
-import Header from "./components/Header/Header";
-import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
 import AppRoutes from "./routes";
 
 export default function App() {
-  const { isAuthenticated } = useAuth();
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    if (
-      !isAuthenticated &&
-      location.pathname !== "/login" &&
-      location.pathname !== "/signup"
-    ) {
-      navigate("/login");
-    }
-  }, [isAuthenticated, location.pathname, navigate]);
-
   return (
     <>
-      {isAuthenticated && <Header />}
       <AppRoutes />
     </>
   );
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,12 +1,32 @@
-import { Routes } from "react-router-dom";
+import { Routes, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useAuth } from "../context/AuthContext/AuthContext";
 import AuthRoutes from "./AuthRoutes";
 import DashboardRoutes from "./DashboardRoutes";
+import Header from "../components/Header/Header";
 
 export default function AppRoutes() {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const isAuthPage = ["/login", "/signup"].includes(location.pathname);
+    if (!isAuthenticated && !isAuthPage) navigate("/login");
+  }, [isAuthenticated, location.pathname, navigate]);
+
+  const shouldApplyPadding =
+    isAuthenticated && !["/login", "/signup"].includes(location.pathname);
+
   return (
-    <Routes>
-      {AuthRoutes()}
-      {DashboardRoutes()}
-    </Routes>
+    <>
+      {isAuthenticated && <Header />}
+      <Routes>{AuthRoutes()}</Routes>
+      {isAuthenticated && (
+        <div className={shouldApplyPadding ? "px-5 py-5" : ""}>
+          <Routes>{DashboardRoutes()}</Routes>
+        </div>
+      )}
+    </>
   );
 }
